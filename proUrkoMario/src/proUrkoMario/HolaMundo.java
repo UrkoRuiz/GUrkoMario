428a28bbfca53d3db0cf5a1465d6ade8a9260f12
Urko <Urko@DESKTOP-HL5543P>
2023-04-15 13:11:31 +0200

    version 2

--- proUrkoMario/src/proUrkoMario/HolaMundo.java
+++ proUrkoMario/src/proUrkoMario/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Adios Mundo");
+		System.out.println("Hola Mundo");
 	}
 
 }
